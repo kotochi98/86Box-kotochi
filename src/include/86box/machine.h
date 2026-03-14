1c79e06eba93b20bbbc1d8d3d190bf100c715ac1
kotochi98 <185547947+kotochi98@users.noreply.github.com>
2026-03-14 16:06:44 +0300

    Preliminary implementation of the Advanced Integration Research (AIR) P6KDI

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -1221,9 +1221,9 @@ extern int             machine_at_m729_init(const machine_t *);
 
 /* i440FX */
 extern int             machine_at_acerv62x_init(const machine_t *);
+extern int             machine_at_p6kdi_init(const machine_t *);
 extern int             machine_at_p65up5_cpknd_init(const machine_t *);
 extern int             machine_at_kn97_init(const machine_t *);
-extern int             machine_at_fickn6010_init(const machine_t *);
 
 /* i440LX */
 #ifdef EMU_DEVICE_H
--- src/machine/m_at_slot1.c
+++ src/machine/m_at_slot1.c
@@ -104,6 +104,37 @@ machine_at_acerv62x_init(const machine_t *model)
 }
 
 int
+machine_at_p6kdi_init(const machine_t *model)
+{
+    int ret;
+
+    ret = bios_load_linear("roms/machines/p6kdi/p6kdi20y.rom",
+                           0x000c0000, 262144, 0);
+
+    if (bios_only || !ret)
+        return ret;
+
+    machine_at_common_init(model);
+
+    pci_init(PCI_CONFIG_TYPE_1);
+    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
+    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 0, 0, 0, 4);
+    pci_register_slot(0x08, PCI_CARD_SCSI,        4, 0, 0, 0); /* assumed */
+    pci_register_slot(0x09, PCI_CARD_NORMAL,      1, 2, 3, 4);
+    pci_register_slot(0x0A, PCI_CARD_NORMAL,      4, 1, 2, 3);
+    pci_register_slot(0x0B, PCI_CARD_NORMAL,      3, 4, 1, 2);
+    pci_register_slot(0x0C, PCI_CARD_NORMAL,      2, 3, 4, 1);
+    pci_register_slot(0x0D, PCI_CARD_NORMAL,      1, 2, 3, 4);
+
+    device_add(&i440fx_device);
+    device_add(&piix3_device);
+    device_add_params(&fdc37c93x_device, (void *) (FDC37XXX2 | FDC37C93X_NORMAL | FDC37C93X_NO_NVR));
+    device_add(&intel_flash_bxt_device);
+
+    return ret;
+}
+
+int
 machine_at_p65up5_cpknd_init(const machine_t *model)
 {
     int ret;
@@ -149,41 +180,6 @@ machine_at_kn97_init(const machine_t *model)
     device_add(&lm78_device); /* fans: Chassis, CPU, Power; temperature: MB */
     for (uint8_t i = 0; i < 3; i++)
         hwm_values.fans[i] *= 2; /* BIOS reports fans with the wrong divisor for some reason */
-
-    return ret;
-}
-
-int
-machine_at_fickn6010_init(const machine_t *model)
-{
-    int ret;
-
-    ret = bios_load_linear("roms/machines/fickn6010/626hb13.rom",
-                           0x000e0000, 131072, 0);
-
-    if (bios_only || !ret)
-        return ret;
-
-    machine_at_common_init(model);
-
-    pci_init(PCI_CONFIG_TYPE_1);
-    pci_register_slot(0x00, PCI_CARD_NORTHBRIDGE, 0, 0, 0, 0);
-    pci_register_slot(0x07, PCI_CARD_SOUTHBRIDGE, 1, 2, 3, 4);
-    pci_register_slot(0x08, PCI_CARD_NORMAL,      1, 2, 3, 4);
-    pci_register_slot(0x09, PCI_CARD_NORMAL,      2, 3, 4, 1);
-    pci_register_slot(0x0A, PCI_CARD_NORMAL,      3, 4, 1, 2);
-    pci_register_slot(0x0B, PCI_CARD_NORMAL,      4, 1, 2, 3);
-    pci_register_slot(0x0C, PCI_CARD_NORMAL,      4, 1, 2, 3);
-
-    device_add(&i440fx_device);
-    device_add(&piix3_device);
-    /* This actually has the W83977AF, which seems to be same as
-       the W83977F but with IrDA FIR (Fast Infrared) support */
-    device_add_params(&w83977_device, (void *) (W83977F | W83977_370 | W83977_AMI | W83977_NO_NVR));
-    /* From the very blurry picture, it looks like an SST flash chip.
-       And indeed, the Micro House schema lists a jumper that selects
-       Intel or SST flash, which specifically mentions the 29EE010 */
-    device_add(&sst_flash_29ee010_device);
 
     return ret;
 }
